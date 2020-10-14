1e87b210a208505ba8bcfec943310b523a0484c9
suman <Sumanstm21@gmail.com>
2020-10-14 21:33:30 +0200

    Updates

---
+++ precache-manifest.f2586bfc47ef020946d7d4afeb3fc447.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c80eb91fba28d0aeae79f11aa14a7f55",
+    "url": "/Kea-mandatory-react/index.html"
+  },
+  {
+    "revision": "952dd66d0894171b9d2b",
+    "url": "/Kea-mandatory-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1cb28211ed349d47f23b",
+    "url": "/Kea-mandatory-react/static/js/2.43153eda.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/Kea-mandatory-react/static/js/2.43153eda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "952dd66d0894171b9d2b",
+    "url": "/Kea-mandatory-react/static/js/main.173bdb9a.chunk.js"
+  },
+  {
+    "revision": "33de7db3efee71064565",
+    "url": "/Kea-mandatory-react/static/js/runtime-main.21eb8b6e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Kea-mandatory-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
